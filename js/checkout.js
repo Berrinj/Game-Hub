5243ad9e9004b02b003cd34b20e87cb7f9adadac
Berrinj <69366646+Berrinj@users.noreply.github.com>
2023-11-12 20:26:11 +0100

    last changes

---
+++ js/checkout.js
@@ -0,0 +1,28 @@
+import { getCartItems } from "./utils/getCartItems.js";
+
+const main = document.querySelector("main");
+const productContainer = document.querySelector(".checkout-items");
+const itemsInCart = getCartItems();
+
+productContainer.innerHTML = "";
+
+try {
+
+if(itemsInCart.length === 0) {
+    productContainer.innerHTML = `<p>No items in cart.</p>`;
+};
+
+itemsInCart.forEach(inCart => {
+
+productContainer.innerHTML += `<div class="checkout-items">
+                                <img src="${inCart.image}" alt="${inCart.title} cover" class="checkout-img">
+                                <p><b>Title:</b>${inCart.title}</p>
+                                <p><b>Quantity:</b> 1</p>
+                                <p><b>Price:</b>${inCart.price}</p>
+                                <hr>
+                                </div>`;
+});
+} catch (error) {
+        main.innerHTML = `<div class="error">We are so sorry, an error occured while loading this page.</div>`;
+        console.log(error, `Sorry, an error occured`);
+};
